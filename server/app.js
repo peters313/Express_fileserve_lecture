92e2ca60c1f4dd719fcbe54e5414cf600029633c
peters313 <ericpeters@Erics-MacBook-Pro.local>
2015-07-30 10:09:46 -0500

    changed formatting

--- server/app.js
+++ server/app.js
@@ -4,8 +4,13 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, "./public/views/index.html"));
+app.get('/data-request', function(req, res){
+   res.json({message: "Hello"});
+});
+
+app.get('/*', function(req, res){
+    var file = req.params[0] || "views/index.html";
+    res.sendFile(path.join(__dirname, "./public/", file));
 });
 
 app.listen(app.get('port'), function(){
